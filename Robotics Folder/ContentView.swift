be55ae5f2ae42ae0abcd6f6907c4f48803087f10
Caedmon Myers <caedmonmyers@icloud.com>
2024-12-18 15:03:38 -0600

    Initial Commit

---
+++ Robotics Folder/ContentView.swift
@@ -0,0 +1,27 @@
+//
+// Robotics Folder
+// ContentView.swift
+//
+// Created on 18/12/24
+//
+// Copyright ©2024 DoorHinge Apps.
+//
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Robotics Folder/Robotics_FolderApp.swift
@@ -0,0 +1,20 @@
+//
+// Robotics Folder
+// Robotics_FolderApp.swift
+//
+// Created on 18/12/24
+//
+// Copyright ©2024 DoorHinge Apps.
+//
+
+
+import SwiftUI
+
+@main
+struct Robotics_FolderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
